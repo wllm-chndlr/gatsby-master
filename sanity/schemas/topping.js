4ce717965c7e76867081066d390d2dbf0d3a999e
William Chandler <chndlr.wllm@gmail.com>
2020-10-28 19:56:42 -0500

    toppings schema

---
+++ sanity/schemas/topping.js
@@ -0,0 +1,32 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+    name: 'topping',
+    title: 'Toppings',
+    type: 'document',
+    icon,
+    fields: [
+        {
+            name: 'name',
+            title: 'Topping Name',
+            type: 'string',
+            description: 'Name of the topping',
+        },
+        {
+            name: 'vegetarian',
+            title: 'Vegetarian',
+            type: 'boolean',
+            description: 'Name of the topping',
+        },
+
+    ],
+    preview: {
+        select: {
+            name: 'name',
+            vegetarian: 'vegetarian',
+        },
+        prepare: ({name, vegetarian}) => ({
+            title: `${name} ${vegetarian ? '🌱' : ''}`,
+        })
+    }
+}
