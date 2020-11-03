ce32e07fc737c6bdfdfb1f7563e7fc5f32d871c2
William Chandler <chndlr.wllm@gmail.com>
2020-11-02 19:49:00 -0600

    update pizza schema

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -37,5 +37,30 @@ export default {
             validation: Rule => Rule.min(1000),
             // TODO: add custom component
         },
+        {
+            name: 'toppings',
+            title: 'Toppings',
+            type: 'array',
+            of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+        },
     ],
+    preview: {
+        select: {
+            title: 'name',
+            media: 'image',
+            topping0: 'toppings.0.name',
+            topping1: 'toppings.1.name',
+            topping2: 'toppings.2.name',
+            topping3: 'toppings.3.name',
+        },
+        prepare: ({ title, media, ...toppings }) => {
+            const tops = Object.values(toppings).filter(Boolean);
+
+            return {
+                title,
+                media,
+                subtitle: tops.join(', '),
+            }
+        },
+    }
 }
