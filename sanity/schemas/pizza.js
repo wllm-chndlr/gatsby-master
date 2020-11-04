7f2fac87238ad0c11ce30dd55767b26db4e45983
William Chandler <chndlr.wllm@gmail.com>
2020-11-04 13:49:05 -0600

    custom sanity price input, new person schema

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
     name: 'pizza',
@@ -36,6 +37,7 @@ export default {
             description: 'Price of the pizza in cents',
             validation: Rule => Rule.min(1000),
             // TODO: add custom component
+            inputComponent: PriceInput,
         },
         {
             name: 'toppings',
