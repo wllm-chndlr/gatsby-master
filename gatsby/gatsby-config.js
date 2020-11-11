02c9c758c6d646bf9151845e4c8af5ee9e07e4ec
William Chandler <chndlr.wllm@gmail.com>
2020-11-10 19:42:51 -0600

    gatsby source sanity

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,7 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
     siteMetadata: {
         title: 'Slicks Slices',
         siteUrl: 'https://gatsby.pizza',
         description: 'The best pizza place in Austin',
     },
+    plugins: [
+        'gatsby-plugin-styled-components',
+        {
+            resolve: 'gatsby-source-sanity',
+            options: {
+                projectId: 'xgeovjml',
+                dataset: 'production',
+                watchMode: true,
+                token: process.env.SANITY_TOKEN,
+            }
+        }
+    ],
 };
