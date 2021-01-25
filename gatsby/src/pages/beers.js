92c20c90af015e288d841acc6aac4ad35a6062a1
William Chandler <chndlr.wllm@gmail.com>
2021-01-25 17:48:52 -0600

    querying displaying styling beers page (part 1)

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,5 +1,51 @@
 import React from "react";
+import { graphql } from 'gatsby';
 
-export default function BeersPage() {
-	return <p>Hey! I'm the Beers page</p>;
+export default function BeersPage({ data }) {
+	return (
+        <>
+            <h2 className="center">
+                We have {data.beers.nodes.length} beers available! Dine-in only.
+            </h2>
+
+            <div>
+                {data.beers.nodes.map(beer => {
+                    const rating = Math.round(beer.rating.average);
+
+                    return (
+                        <div key={beer.id}>
+                            <img src={beer.image} alt={beer.name}/>
+                            <h3>
+                                {beer.name}
+                            </h3>
+                            {beer.price}
+                            <p title={`${rating} out of 5 stars`}>
+                                {`*`.repeat(rating)}
+                                <span style={{ filter: `grayscale(100%)` }}>
+                                    {`*`.repeat(5 - rating)}
+                                </span>
+                            </p>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    );
 }
+
+export const query = graphql`
+    query {
+        beers: allBeer {
+            nodes {
+                id
+                name
+                price
+                image
+                rating {
+                    average
+                    reviews
+                }
+            }
+        }
+    }
+`;
