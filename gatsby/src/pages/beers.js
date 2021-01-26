ded807d502bd8594ec89462ede819f2910c4b3ee
William Chandler <chndlr.wllm@gmail.com>
2021-01-26 14:44:51 -0600

    beers (part 2)

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,5 +1,27 @@
 import React from "react";
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
+
+const BeerGridStyles = styled.div`
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+const SingleBeerStyles = styled.div`
+    border: 1px solid var(--grey);
+    padding: 2rem;
+    text-align: center;
+    img {
+        width: 100%;
+        height: 200px;
+        object-fit: contain;
+        display: block;
+        display: grid;
+        align-items: center;
+        font-size: 10px;
+    }
+`;
 
 export default function BeersPage({ data }) {
 	return (
@@ -8,27 +30,28 @@ export default function BeersPage({ data }) {
                 We have {data.beers.nodes.length} beers available! Dine-in only.
             </h2>
 
-            <div>
+            <BeerGridStyles>
                 {data.beers.nodes.map(beer => {
                     const rating = Math.round(beer.rating.average);
 
                     return (
-                        <div key={beer.id}>
+                        <SingleBeerStyles key={beer.id}>
                             <img src={beer.image} alt={beer.name}/>
                             <h3>
                                 {beer.name}
                             </h3>
                             {beer.price}
                             <p title={`${rating} out of 5 stars`}>
-                                {`*`.repeat(rating)}
+                                {`⭐`.repeat(rating)}
                                 <span style={{ filter: `grayscale(100%)` }}>
-                                    {`*`.repeat(5 - rating)}
+                                    {`⭐`.repeat(5 - rating)}
                                 </span>
+                                <span>({beer.rating.reviews})</span>
                             </p>
-                        </div>
+                        </SingleBeerStyles>
                     )
                 })}
-            </div>
+            </BeerGridStyles>
         </>
     );
 }
