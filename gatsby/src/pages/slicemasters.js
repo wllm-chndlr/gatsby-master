a1aed1a54b0f31d26640b321b41d35e4cf0ae10f
William Chandler <chndlr.wllm@gmail.com>
2021-01-27 17:15:01 -0600

    initial person query

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,5 +1,48 @@
 import React from "react";
+import { graphql } from 'gatsby';
+import { Link } from "gatsby";
 
-export default function SlicemastersPage() {
-	return <p>Hey! I'm the Slicemasters page</p>
+export default function SlicemastersPage({ data }) {
+    const slicemasters = data.slicemasters.nodes;
+
+	return (
+        <>
+            <div>
+                {slicemasters.map(person => (
+                    <div>
+                        <Link to={`/slicemaster/${person.slug.current}`}>
+                            <h2>
+                                <span className="mark">{person.name}</span>
+                            </h2>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+
+        </>
+    )
 }
+
+export const query = graphql`
+    query {
+        slicemasters: allSanityPerson {
+            totalCount
+            nodes {
+                name
+                id
+                slug {
+                    current
+                }
+                description
+                image {
+                    asset {
+                        fluid(maxWidth: 410) {
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+`;
