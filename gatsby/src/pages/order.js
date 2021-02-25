6d80a8ba5832d817cb6b118d94a70d458440a137
William Chandler <chndlr.wllm@gmail.com>
2021-02-24 19:32:55 -0600

    order page w/ custom hooks

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,5 +1,37 @@
 import React from "react";
+import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
 
 export default function OrderPage() {
-	return <p>Hey! I'm the Order page</p>;
+    const { values, updateValue } = useForm({
+        name: '',
+        email: '',
+    });
+
+	return (
+        <>
+            <SEO title="Order a pizza!" />
+            <form>
+                <fieldset>
+                    <legend>Your Info</legend>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={values.name}
+                        onChange={updateValue}
+                    />
+
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" />
+                </fieldset>
+                <fieldset>
+                    <legend>Menu</legend>
+                </fieldset>
+                <fieldset>
+                    <legend>Order</legend>
+                </fieldset>
+            </form>
+        </>
+    )
 }
---
+++ gatsby/src/utils/useForm.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export default function useForm(defaults) {
+    const [values, setValues] = useState(defaults)
+
+    function updateValue(e) {
+        // check if it's a number and convert
+        let { value } = e.target;
+
+        if (e.target.type === 'number') {
+            value = parseInt(e.target.value);
+        }
+
+        setValues({
+            // copy existing values into it
+            ...values,
+            // update new value that changed
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    return { values, updateValue };
+}
