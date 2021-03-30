c299de4bace04c0ab9471312ffd4c1d705e500a5
William Chandler <chndlr.wllm@gmail.com>
2021-03-30 16:32:59 -0500

    custom hook for order form

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -7,13 +7,19 @@ import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import calculatePizzaPrice from '../utils/calculatePizzaPrice';
 import formatMoney from '../utils/formatMoney';
+import usePizza from '../utils/usePizza';
+import PizzaOrder from "../components/PizzaOrder";
 
 export default function OrderPage({ data }) {
+    const pizzas = data.pizzas.nodes;
     const { values, updateValue } = useForm({
         name: '',
         email: '',
     });
-    const pizzas = data.pizzas.nodes;
+    const { order, addToOrder, removeFromOrder } = usePizza({
+        pizzas,
+        inputs: values,
+    });
 
 	return (
         <>
@@ -54,7 +60,10 @@ export default function OrderPage({ data }) {
                             </div>
                             <div>
                                 {['S', 'M', 'L'].map(size => (
-                                    <button type="button">
+                                    <button type="button" onClick={() => addToOrder({
+                                        id: pizza.id,
+                                        size,
+                                    })}>
                                         {size}
                                         {formatMoney(calculatePizzaPrice(pizza.price, size))}
                                     </button>
@@ -66,6 +75,11 @@ export default function OrderPage({ data }) {
                 </fieldset>
                 <fieldset className="order">
                     <legend>Order</legend>
+                    <PizzaOrder
+                        order={order}
+                        removeFromOrder={removeFromOrder}
+                        pizzas={pizzas}
+                    />
                 </fieldset>
             </OrderStyles>
         </>
--- gatsby/src/utils/usePizza.js
+++ gatsby/src/utils/usePizza.js
@@ -1,3 +1,25 @@
-export default function usePizza({ pizzas, inputs }) {
+import { useState } from 'react';
 
+export default function usePizza({ pizzas, inputs }) {
+    const [order, setOrder] = useState([]);
+
+    function addToOrder(orderedPizza) {
+        setOrder([...order, orderedPizza]);
+    }
+
+    function removeFromOrder(index) {
+        setOrder([
+            // everything before the item we want to remove
+            ...order.slice(0, index),
+            ...order.slice(index + 1)
+            // everything after the item we want to remove
+        ])
+    }
+
+    // TODO
+    return {
+        order,
+        addToOrder,
+        removeFromOrder,
+    }
 }
