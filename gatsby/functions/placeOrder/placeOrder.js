07e967f76bb09392ded616f31f2cc3bf81ad6a7f
William Chandler <chndlr.wllm@gmail.com>
2021-04-13 19:55:07 -0500

    serverless order function (cont)

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -2,10 +2,24 @@ const nodemailer = require('nodemailer');
 
 // create a transport for nodemailer
 const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
+    host: process.env.MAIL_HOST,
     port: 587,
     auth: {
-        user: 'geraldine92@ethereal.email',
-        pass: 'mzQDV8z81mGCAfHArw',
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
     }
 })
+
+exports.handler = async (event, context) => {
+    const info = await transporter.sendMail({
+        from: 'Slicks Slices <slick@example.com>',
+        to: 'orders@example.com',
+        subject: 'New order!',
+        html: `<p>Your new pizza order is here!</p>`,
+    });
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(info),
+    }
+}
