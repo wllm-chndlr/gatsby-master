a649da522e4677573a913ccdbe58ae47073aa07c
William Chandler <chndlr.wllm@gmail.com>
2021-09-01 16:37:53 -0700

    update index.js / check github auth

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function HomePage() {
-	return <p>Hey! I'm the Home page</p>;
+
+    return (
+        <div className="center">
+            <h1>
+                The Best Pizza Downtown!
+            </h1>
+            <p>Open 11am to 11pm Every Single Day</p>
+        </div>
+    )
 }
