d270510e4e8fe23228a903e005efb4bc02834aae
William Chandler <chndlr.wllm@gmail.com>
2021-11-11 16:37:10 -0800

    updating responsiveness

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,78 +1,110 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from 'react';
+import { Link } from 'gatsby';
 import styled from 'styled-components';
-import Logo from "./Logo";
+import Logo from './Logo';
 
 const NavStyles = styled.nav`
-    .logo {
-        transform: translateY(-25%);
+  .logo {
+    transform: translateY(-25%);
+  }
+
+  ul {
+    align-items: center;
+    display: grid;
+    grid-gap: 2rem;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    list-style: none;
+    margin: 0;
+    margin-top: -6rem;
+    padding: 0;
+    text-align: center;
+  }
+
+  li {
+    order: 1;
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+
+    &:nth-child(1) {
+      --rotate: 1deg;
     }
+
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+  a {
+    display: block;
+    font-size: 3rem;
+    text-decoration: none;
+
+    &:hover {
+      color: var(--red);
+    }
+
+    @media (max-width: 800px) {
+      font-size: 2rem;
+    }
+  }
+
+  @media (max-width: 600px) {
+    --columns: 4;
+
+    margin-bottom: 2rem;
+    border-bottom: 2px solid var(--grey);
+    padding-bottom: 2rem;
 
     ul {
-        align-items: center;
-        display: grid;
-        grid-gap: 2rem;
-        grid-template-columns: 1fr 1fr auto 1fr 1fr;
-        list-style: none;
-        margin: 0;
-        margin-top: -6rem;
-        padding: 0;
-        text-align: center;
+      grid-template-rows: auto auto;
+      grid-template-columns: repeat(var(--columns), 1fr);
+      justify-items: center;
     }
 
-    li {
-        order: 1;
-        --rotate: -2deg;
-        transform: rotate(var(--rotate));
-
-        &:nth-child(1) {
-            --rotate: 1deg;
-        }
-
-        &:nth-child(2) {
-            --rotate: -2.5deg;
-        }
-
-        &:nth-child(4) {
-            --rotate: 2.5deg;
-        }
-
-        &:hover {
-            --rotate: 3deg;
-        }
+    .logo-item {
+      order: 0;
+      grid-column: 1 / -1;
     }
-    a {
-        font-size: 3rem;
-        text-decoration: none;
 
-        &:hover {
-            color: var(--red);
-        }
+    .logo {
+      transform: none;
     }
+  }
+
+  @media (max-width: 500px) {
+    --columns: 2;
+  }
 `;
 
 export default function Nav() {
-	return (
-		<NavStyles>
-			<ul>
-				<li>
-					<Link to="/">Hot Now</Link>
-				</li>
-				<li>
-					<Link to="/pizzas">Pizza Menu</Link>
-				</li>
-                <li>
-					<Link to="/">
-                        <Logo />
-                    </Link>
-				</li>
-                <li>
-					<Link to="/slicemasters">SliceMasters</Link>
-				</li>
-                <li>
-					<Link to="/order">Order Ahead!</Link>
-				</li>
-			</ul>
-		</NavStyles>
-	);
+  return (
+    <NavStyles>
+      <ul>
+        <li>
+          <Link to="/">Hot Now</Link>
+        </li>
+        <li>
+          <Link to="/pizzas">Pizza Menu</Link>
+        </li>
+        <li className="logo-item">
+          <Link to="/">
+            <Logo />
+          </Link>
+        </li>
+        <li>
+          <Link to="/slicemasters">SliceMasters</Link>
+        </li>
+        <li>
+          <Link to="/order">Order Ahead!</Link>
+        </li>
+      </ul>
+    </NavStyles>
+  );
 }
